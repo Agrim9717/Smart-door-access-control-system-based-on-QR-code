b1709f87bee4c5c7da947fdbbd7c611bb9b9908a
Agrim Jain <101503131+Agrim9717@users.noreply.github.com>
2023-07-31 22:24:27 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data1_df = pd.read_csv('login_data.csv')
+data2_df = pd.read_csv('final_data.csv')
+data3_df = pd.read_csv('full_data.csv')
+# print(data1_df)
+#print(data2_df)
+print(data3_df)
